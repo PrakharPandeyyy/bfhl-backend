aebd3643805ea3a7cd0a48ac15ea4e041263fc59
prakharpandeyyy <8.prakharpandey@gmail.com>
2024-09-22 17:10:09 +0530

    added cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const cors = require("cors");
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json({ limit: "10mb" }));
